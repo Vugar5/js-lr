45eb8d6ca59f146a4b3780124dce174a0f36af65
grasth <grasth@github.com>
2019-04-16 16:01:06 +0400

    /

---
+++ src/main.js
@@ -0,0 +1,23 @@
+var req = new XMLHttpRequest();
+req.open('GET', 'http://www.omdbapi.com/?apikey=202de9b6&s=batman', false); 
+req.send(null);
+if(req.status == 0)
+  dump(req.responseText);
+  let result = JSON.parse(req.responseText).Search;
+  result.forEach(element => {
+      console.log(element.Poster);
+  });
+
+
+  let movie = document.getElementById('movie');
+
+//   result.forEach(element => {
+//     img.src=(element.Poster);
+// });
+
+ 
+ 
+ 
+//   <img src="element.Poster"></img>
+//   <img src=""></img>
+//   <img src=""></img>
