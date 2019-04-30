3da3760e7e876e6063641d2642b64ae6e9e16d85
grasth <grasth@github.com>
2019-04-30 15:11:55 +0400

    1

--- src/main.js
+++ src/main.js
@@ -1,25 +1,53 @@
-var req = new XMLHttpRequest();
-req.open('GET', 'http://www.omdbapi.com/?apikey=202de9b6&s=spider-man', false); 
-req.send(null);
-if(req.status == 0)
-  dump(req.responseText);
-  let result = JSON.parse(req.responseText).Search;
-  result.forEach(element => {
-      console.log(element.Poster);
+
+function Zapros() {
+  return new Promise((resolve,reject)=>{
+    var req = new XMLHttpRequest();
+    req.open('GET', 'http://www.omdbapi.com/?&apikey=202de9b6&s=spider-man', false);
+    req.send(null);
+    if(req.status!=200) ()=>reject (()=>console.log("Что-то пошло не так"));
+    let result = JSON.parse(req.responseText).Search;
+    resolve(result);
+    console.log(result);
   });
+}
+
+ // var req = new XMLHttpRequest();
+  // req.open('GET', 'http://www.omdbapi.com/?&apikey=202de9b6&s=spider-man', false);
+  // req.send(null);
+  // if (req.status == 200) {
+
+  //   let result = JSON.parse(req.responseText).Search;
+
+  //   //resolve(result.Search);
+  //   result.forEach(element => {
+  //     console.log(element.Poster);
+  //   });
 
 
-  let movie = document.getElementById('movie');
+  // }
 
-  
+// let movie = document.getElementById('movie');
 
+function render(movieList) {
+  let movie = document.getElementById('movies');
+  movieList.forEach(item => {
+    let img = document.createElement('img');
+    img.src = item.Poster;
+    movie.appendChild(img);
+  });
+}
+
+Zapros()
+  .then(result=>render(result))
+  .catch(e => console.log(e));
 //   result.forEach(element => {
 //     img.src=(element.Poster);
 // });
 
- 
- 
- 
-//   <img src="element.Poster"></img>
-//   <img src=""></img>
-//   <img src=""></img>
+
+
+
+  // <img src="element.Poster"></img>
+  // <img src=" "></img>
+  // <img src=""></img>
+
